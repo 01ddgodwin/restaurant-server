47810c638f6fb0587505dbf0c6ac9e962b34d1ff
Dillon Godwin <59660523+01ddgodwin@users.noreply.github.com>
2023-12-12 15:41:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Define your API endpoint to fetch restaurants from Yelp
+app.get('/api/restaurants', async (req, res) => {
+  try {
+    const { latitude, longitude, radius } = req.query;
+    const apiKey = process.env.YELP_API_KEY; // Replace with your Yelp API key
+
+    // Make a request to Yelp API
+    const response = await axios.get('https://api.yelp.com/v3/businesses/search', {
+      params: {
+        latitude,
+        longitude,
+        term: 'restaurants', // Adjust query parameters as needed
+        limit: 20,
+        radius: radius * 1609
+      },
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data.businesses);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

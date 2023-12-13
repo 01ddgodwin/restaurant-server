881d80c395f61215445806203c7428e760fbaaff
Dillon Godwin <59660523+01ddgodwin@users.noreply.github.com>
2023-12-13 00:07:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 // Define your API endpoint to fetch restaurants from Yelp
 app.get('/api/restaurants', async (req, res) => {
   try {
-    const { latitude, longitude, radius, open_now, price } = req.query;
+    const { latitude, longitude, radius, open_now, price, categories } = req.query;
     const apiKey = process.env.YELP_API_KEY; // Replace with your Yelp API key
 
     // Make a request to Yelp API
@@ -23,7 +23,8 @@ app.get('/api/restaurants', async (req, res) => {
         limit: 20,
         radius: radius * 1609,
         open_now,
-        price 
+        price,
+        categories
       },
       headers: {
         Authorization: `Bearer ${apiKey}`,
